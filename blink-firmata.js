1c48ea81d77ee88e63a77dcbae2f6290af62f2f7
marcus <WittigMarcus@gmail.com>
2016-01-25 00:59:21 +0100

    Initial implementation

---
+++ blink-firmata.js
@@ -0,0 +1,18 @@
+var Firmata = require("firmata").Board;
+var EtherPort = require("etherport");
+var board = new Firmata(new EtherPort(3030));
+
+board.on("ready", function() {
+    console.log("ready");
+
+    var state = 1;
+    this.pinMode(8, this.MODES.OUTPUT);
+
+    setInterval(function() {
+        this.digitalWrite(42, (state ^= 1));
+    }.bind(this), 500);
+});
+
+board.on("error", function(error) {
+    console.log("error", error);
+});
